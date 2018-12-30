7c0c910c5cf8078d2f9935f408fd92db68f23b2f
Winnipass <adams4lyf@gmail.com>
2018-12-30 13:38:13 +0000

    en(ui): broke down ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavBarComponent, SideMenuComponent,
+    ContentComponent, FooterComponent
   ],
   imports: [
     BrowserModule
